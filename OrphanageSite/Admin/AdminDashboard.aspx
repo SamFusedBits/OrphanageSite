64ae7e4863873af19f409c50a5d6f306018957c4
M Sharique Siddiqui <157228213+m-sharique@users.noreply.github.com>
2024-05-26 22:43:14 +0530

    Created Admin Master Page and Started wroking on Admin Dashboard

---
+++ OrphanageSite/Admin/AdminDashboard.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin/AdminMaster.Master" AutoEventWireup="true" CodeBehind="AdminDashboard.aspx.cs" Inherits="OrphanageSite.Admin.AdminDashboard" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <style>
+        body{
+            height: 100vh;
+            width: 100vw;
+        }
+    </style>
+    <body>
+        <h1>Dashboard</h1>
+    </body>
+</asp:Content>
---
+++ OrphanageSite/Admin/AdminMaster.Master.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OrphanageSite.Admin
+{
+    public partial class AdminMaster : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Your code here
+        }
+
+        protected void btnLogout_Click(object sender, EventArgs e)
+        {
+            // Clear the session
+            Session.Clear();
+            // Redirect to the AdminLogin page
+            Response.Redirect("~/AdminLogin.aspx");
+        }
+    }
+}
