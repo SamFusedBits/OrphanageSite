8fe237978ef437351e442fa2d97c09e4ddce633a
M Sharique Siddiqui <157228213+m-sharique@users.noreply.github.com>
2024-05-28 20:17:40 +0530

    Created and Completed Adoption Status Page

---
+++ OrphanageSite/Admin/AdoptionStatus.aspx
@@ -0,0 +1,58 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin/AdminMaster.Master" AutoEventWireup="true" CodeBehind="AdoptionStatus.aspx.cs" Inherits="OrphanageSite.Admin.AdoptionStatus" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <style>
+        .container{
+            margin: 35px auto;
+        }
+        .card-body{
+            padding: 20px;
+        }
+        hr{
+            width: 50%;
+            margin: auto;
+        }
+        .pending {
+            color: red;
+            font-weight: bold;
+        }
+        .approved {
+            color: green;
+            font-weight: bold;
+        }
+        .declined {
+            color: grey;
+            font-weight: bold;
+        }
+    </style>
+    <div class="container mt-5 card">
+            <h2 class="card-header">Adoption Status</h2>
+            <div class="card-body">
+                <asp:GridView ID="GridViewAdoptions" runat="server" AutoGenerateColumns="False" CssClass="table table-striped">
+                    <Columns>
+                        <asp:BoundField DataField="AdoptionID" HeaderText="Adoption ID" />
+                        <asp:BoundField DataField="ChildID" HeaderText="Child ID" />
+                        <asp:BoundField DataField="ChildFirstName" HeaderText="Child First Name" />
+                        <asp:BoundField DataField="ChildLastName" HeaderText="Child Last Name" />
+                        <asp:BoundField DataField="UserID" HeaderText="User ID" />
+                        <asp:BoundField DataField="UserFirstName" HeaderText="User First Name" />
+                        <asp:BoundField DataField="UserLastName" HeaderText="User Last Name" />
+                        <asp:BoundField DataField="AdoptionDate" HeaderText="Adoption Date" DataFormatString="{0:yyyy-MM-dd}" />
+                        <asp:TemplateField HeaderText="Status">
+                            <ItemTemplate>
+                                <asp:Label ID="lblStatus" runat="server" Text='<%# Eval("Status") == DBNull.Value ? "Pending" : (bool)Eval("Status") ? "Approved" : "Declined" %>' 
+                                           CssClass='<%# Eval("Status") == DBNull.Value ? "pending" : (bool)Eval("Status") ? "approved" : "declined" %>'></asp:Label>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                        <asp:TemplateField>
+                            <ItemTemplate>
+                                <asp:Button ID="btnApprove" runat="server" Text="Approve" CommandArgument='<%# Eval("AdoptionID") %>' OnClick="btnApprove_Click" CssClass="btn btn-success" />
+                                <asp:Button ID="btnDecline" runat="server" Text="Decline" CommandArgument='<%# Eval("AdoptionID") %>' OnClick="btnDecline_Click" CssClass="btn btn-danger" />
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                    </Columns>
+                </asp:GridView>
+            </div>
+        </div>
+</asp:Content>
---
+++ OrphanageSite/Admin/AdoptionStatus.aspx.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OrphanageSite.Admin
+{
+    public partial class AdoptionStatus : System.Web.UI.Page
+    {
+        private readonly string connStr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\smsha\Desktop\Sharique\MCA\Sem2\Web Technology\Project\OrphanageSite\OrphanageSite\DataBase\OrphanageSiteDB.mdf;Integrated Security=True;Connect Timeout=30";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindGrid();
+            }
+        }
+
+        private void BindGrid()
+        {
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                SqlDataAdapter da = new SqlDataAdapter(@"SELECT A.*, U.FirstName AS UserFirstName, U.LastName AS UserLastName,
+                                                C.FirstName AS ChildFirstName, C.LastName AS ChildLastName
+                                         FROM Adoptions A
+                                         INNER JOIN Users U ON A.UserID = U.UserID
+                                         INNER JOIN Children C ON A.ChildID = C.ChildID", conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                GridViewAdoptions.DataSource = dt;
+                GridViewAdoptions.DataBind();
+            }
+        }
+
+        protected void btnApprove_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            int adoptionId = Convert.ToInt32(btn.CommandArgument);
+
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                SqlTransaction transaction = conn.BeginTransaction();
+                try
+                {
+                    // Approve the selected adoption
+                    SqlCommand cmdApprove = new SqlCommand("UPDATE Adoptions SET Status = 1 WHERE AdoptionID = @AdoptionID", conn, transaction);
+                    cmdApprove.Parameters.AddWithValue("@AdoptionID", adoptionId);
+                    cmdApprove.ExecuteNonQuery();
+
+                    // Find the ChildID of the approved adoption
+                    SqlCommand cmdChildId = new SqlCommand("SELECT ChildID FROM Adoptions WHERE AdoptionID = @AdoptionID", conn, transaction);
+                    cmdChildId.Parameters.AddWithValue("@AdoptionID", adoptionId);
+                    int childId = (int)cmdChildId.ExecuteScalar();
+
+                    // Decline all other adoptions for the same ChildID
+                    SqlCommand cmdDeclineOthers = new SqlCommand("UPDATE Adoptions SET Status = 0 WHERE ChildID = @ChildID AND AdoptionID != @AdoptionID", conn, transaction);
+                    cmdDeclineOthers.Parameters.AddWithValue("@ChildID", childId);
+                    cmdDeclineOthers.Parameters.AddWithValue("@AdoptionID", adoptionId);
+                    cmdDeclineOthers.ExecuteNonQuery();
+
+                    // Commit the transaction
+                    transaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    // Rollback the transaction in case of an error
+                    transaction.Rollback();
+                    // Show error message
+                    ScriptManager.RegisterStartupScript(this, GetType(), "errorAlert", $"alert('An error occurred: {ex.Message}');", true);
+                }
+            }
+            BindGrid();
+        }
+
+        protected void btnDecline_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            int adoptionId = Convert.ToInt32(btn.CommandArgument);
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connStr))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("UPDATE Adoptions SET Status = 0 WHERE AdoptionID = @AdoptionID", conn);
+                    cmd.Parameters.AddWithValue("@AdoptionID", adoptionId);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                // Show error message
+                ScriptManager.RegisterStartupScript(this, GetType(), "errorAlert", $"alert('An error occurred: {ex.Message}');", true);
+            }
+            BindGrid();
+        }
+    }
+}
